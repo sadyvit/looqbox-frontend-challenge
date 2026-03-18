18f924a40def29d0bb3ea4c2ef8b45041c017381
sadyvit <sadyvitor@hotmail.com>
2026-03-18 18:36:49 -0300

    feat(app): folder structure, NavBar and navigation & router stucture

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import { ConfigProvider, Layout } from 'antd';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Pokedex</h1>
-    </div>
-  );
-}
+
+const { Content } = Layout;
+
+const App: React.FC = () => (
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: '#EF5350',
+        borderRadius: 12,
+        fontFamily: "'Nunito', sans-serif",
+      },
+    }}
+  >
+    <BrowserRouter>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Navbar />
+        <Content>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" replace />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Content>
+      </Layout>
+    </BrowserRouter>
+  </ConfigProvider>
+);
 
 export default App;
